2a53a86d0d4a6ace43714d72c4b34eab92dd91de
TetsuroAndo <tetsuro997@gmail.com>
2024-09-11 04:34:10 +0900

    Add install script file

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ chmod +x compile_run.sh
 chmod +x compile_run_w3.sh
 
 current_dir=$(pwd)
-echo "alias cx='$current_dir/compile_run.sh'" >> ~/.zshrc
-echo "alias cwx='$current_dir/compile_run_w3.sh'" >> ~/.zshrc
+echo "alias ccx='$current_dir/compile_run.sh'" >> ~/.zshrc
+echo "alias cx='$current_dir/compile_run_w3.sh'" >> ~/.zshrc
 
 source ~/.zshrc
 
